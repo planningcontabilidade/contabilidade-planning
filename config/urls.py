a1d3832878ed3223e069aa4bc8b43642153aff19
planningcontabilidade <luizcgrodrigues93@gmail.com>
2026-02-17 19:10:53 -0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,23 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth import views as auth_views
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # Home protegida
     path('', views.home, name='home'),
-
-    # Auth padrão Django (usa templates/registration/login.html)
-    path('login/', auth_views.LoginView.as_view(
-        template_name='registration/login.html'
-    ), name='login'),
-
-    path('logout/', auth_views.LogoutView.as_view(
-        next_page='login'
-    ), name='logout'),
-
-    # Criar usuário
-    path('create-user/', views.create_user, name='create_user'),
 ]
