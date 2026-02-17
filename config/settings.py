86abec1a4f448cdd43b1d4ac96a8e040f8ff18d7
planningcontabilidade <luizcgrodrigues93@gmail.com>
2026-02-17 15:41:22 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -71,3 +71,6 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/login/"
