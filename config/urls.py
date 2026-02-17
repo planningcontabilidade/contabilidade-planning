5115a46ecbb3582258108d8989884bf2351c730e
planningcontabilidade <luizcgrodrigues93@gmail.com>
2026-02-17 16:29:15 -0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import home
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('', home, name='home'),
+    path('', views.home, name='home'),
 
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('create-user/', views.create_user),
 ]
-path('create-user/', create_user),
