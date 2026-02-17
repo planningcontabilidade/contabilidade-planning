b50f60352d59aa6dca50c159fb4b22285a5019ef
planningcontabilidade <luizcgrodrigues93@gmail.com>
2026-02-17 20:40:49 -0300

    Update views.py

--- planningcontabilidade/views.py
+++ planningcontabilidade/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.db import connection
+from django.http import HttpResponse
+import os
 
-def home(request):
-    return render(request, "index.html")
+
+def executar_importacao(request):
+    caminho = os.path.join(os.path.dirname(os.path.dirname(__file__)), "import_clientes.sql")
+
+    with open(caminho, "r", encoding="utf-8") as f:
+        sql_script = f.read()
+
+    with connection.cursor() as cursor:
+        cursor.executescript(sql_script)
+
+    return HttpResponse("Importação executada com sucesso!")
