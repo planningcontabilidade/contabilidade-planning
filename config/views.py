dba479f1b9519a805af482ea60b07632d72bc479
planningcontabilidade <luizcgrodrigues93@gmail.com>
2026-02-17 19:09:00 -0300

    Update views.py

--- config/views.py
+++ config/views.py
@@ -1,23 +1,5 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
+from django.shortcuts import render
 
 
-@login_required
 def home(request):
     return render(request, "index.html")
-
-
-def create_user(request):
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser(
-            username="admin",
-            email="admin@email.com",
-            password="123456"
-        )
-        messages.success(request, "Usuário admin criado com sucesso!")
-    else:
-        messages.info(request, "Usuário admin já existe.")
-
-    return redirect("login")
