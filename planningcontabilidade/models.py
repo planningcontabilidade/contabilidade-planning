ac80a1b55638f2aff555d1346a9734fab8296c54
planningcontabilidade <luizcgrodrigues93@gmail.com>
2026-02-17 20:03:55 -0300

    Update models.py

--- planningcontabilidade/models.py
+++ planningcontabilidade/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
-class Empresa(models.Model):
-    nome = models.CharField(max_length=200)
+class Cliente(models.Model):
+    ordem = models.IntegerField(null=True, blank=True)
+    nome = models.CharField(max_length=255)
+    proprietario = models.CharField(max_length=255, null=True, blank=True)
+    cnpj = models.CharField(max_length=20, null=True, blank=True)
+    uf = models.CharField(max_length=5, null=True, blank=True)
+    inscricao_estadual = models.CharField(max_length=50, null=True, blank=True)
+    cpf_responsavel = models.CharField(max_length=20, null=True, blank=True)
+    senha_sefaz = models.CharField(max_length=100, null=True, blank=True)
+    inscricao_municipal = models.CharField(max_length=50, null=True, blank=True)
+    login_prefeitura = models.CharField(max_length=100, null=True, blank=True)
+    senha_prefeitura = models.CharField(max_length=100, null=True, blank=True)
+    codigo_acesso = models.CharField(max_length=100, null=True, blank=True)
+    senha_ecac = models.CharField(max_length=100, null=True, blank=True)
+    observacao = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.nome
