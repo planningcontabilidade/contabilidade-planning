1f5073480e8a7f12915ebaa320e4b19c8118ad33
planningcontabilidade <luizcgrodrigues93@gmail.com>
2026-02-17 16:35:41 -0300

    Update views.py

--- config/views.py
+++ config/views.py
@@ -1,10 +1,16 @@
-    from django.shortcuts import render
+from django.shortcuts import render
 from django.contrib.auth.models import User
+
 
 def home(request):
     return render(request, "index.html")
 
+
 def create_user(request):
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin', 'admin@email.com', '123456')
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            "admin",
+            "admin@email.com",
+            "123456"
+        )
     return render(request, "index.html")
