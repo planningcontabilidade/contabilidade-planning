9663b10fc0a95d60d2718eb6ba2a58cb772181df
planningcontabilidade <luizcgrodrigues93@gmail.com>
2026-02-18 09:48:03 -0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from planningcontabilidade.views import (
-    executar_importacao,
-    executar_migrate,
-    ajustar_nome_nullable,
-)
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('executar-migrate/', executar_migrate),
-    path('ajustar-nome-null/', ajustar_nome_nullable),
-    path('executar-importacao/', executar_importacao),
+    path('', include('planningcontabilidade.urls')),
 ]
