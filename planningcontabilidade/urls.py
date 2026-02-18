1f5332c89c8b6abbdd7c19c2f3dc60b0ceece280
planningcontabilidade <luizcgrodrigues93@gmail.com>
2026-02-18 09:04:12 -0300

    Update urls.py

--- planningcontabilidade/urls.py
+++ planningcontabilidade/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from planningcontabilidade.views import home, executar_importacao
+from django.urls import path, include
 
 urlpatterns = [
-    path('', home),  # Página inicial
-    path('executar-importacao/', executar_importacao),
     path('admin/', admin.site.urls),
+    path('', include('planningcontabilidade.urls')),
 ]
